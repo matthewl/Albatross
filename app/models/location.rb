a9c2b087d7c5e7af3e89e528398a54e972a58a2c
Matthew Lang <matthewlang@hey.com>
2022-08-30 21:24:02 +0100

    Add location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,10 @@
 class Location < ApplicationRecord
   belongs_to :locatable, polymorphic: true
 
+  after_validation :geocode
+
+  geocoded_by :address
+
   def coordinates_present?
     latitude.present? && longitude.present?
   end
