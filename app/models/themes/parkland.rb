ff65fedec89efa1fd22cacad35eccf620ad80d45
Matthew Lang <matthewlang@hey.com>
2022-09-12 20:15:43 +0100

    Parkland theme (#25)
    Start implementation of the Parkland theme with a header and footer.
    
    * Add header background
    * Fix header and add footer
    * Add the initial layout for the Parkland theme
    * Add a Parkland design style for future reference
    
    Closes #17 - Design and implement Parkland theme

---
+++ app/models/themes/parkland.rb
@@ -0,0 +1,41 @@
+module Themes
+  class Parkland
+    BODY_FONT_FAMILY = "Roboto"
+    HEADINGS_FONT_FAMILY = "Roboto Condensed"
+    THEME = "emerald"
+
+    def self.color_options
+      [
+        ["Blue Gray", "blueGray"],
+        ["Cool Gray", "coolGray"],
+        ["Gray", "gray"],
+        ["True Gray", "trueGray"],
+        ["Warm Gray", "warmGray"],
+        ["Red", "red"],
+        ["Orange", "orange"],
+        ["Amber", "amber"],
+        ["Lime", "lime"],
+        ["Green", "green"],
+        ["Emerald", "emerald"],
+        ["Teal", "teal"],
+        ["Cyan", "cyan"],
+        ["Light Blue", "lightBlue"],
+        ["Blue", "blue"],
+        ["Indigo", "indigo"],
+        ["Violet", "violet"],
+        ["Purple", "purple"],
+        ["Fuschia", "fuschia"],
+        ["Pink", "pink"],
+        ["Rose", "rose"]
+      ]
+    end
+
+    def self.body_font_family
+      BODY_FONT_FAMILY.gsub(" ", "+")
+    end
+
+    def self.headings_font_family
+      HEADINGS_FONT_FAMILY.gsub(" ", "+")
+    end
+  end
+end
