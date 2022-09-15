b164fee4528be1a7703381674fd4a097f190b378
Matthew Lang <matthewlang@hey.com>
2022-09-15 22:45:14 +0100

    Add headers section to admin portal

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get "/", to: "dashboard#index", as: :root
 
+    resources :headers, only: %i[edit update]
     resources :websites, only: %i[edit update]
   end
 
