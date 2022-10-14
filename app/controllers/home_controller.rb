4e7685230cf6d0f23f58cd1b58200d2edc45749a
Matthew Lang <matthewlang@hey.com>
2022-10-14 07:38:12 +0100

    Switch current_website for the current website

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,7 @@
 class HomeController < ApplicationController
-  PROVIDER_DOMAIN = "www.albatross.com"
+  before_action :find_website
 
   def index
-    @website = if subdomain_host?
-      Website.find_sole_by(subdomain: request.subdomain)
-    else
-      Website.find_sole_by(url: request.host)
-    end
-
-    render @website.home_view_path, layout: @website.layout_path
-  end
-
-  private
-
-  def subdomain_host?
-    return request.host.include?("lvh.me") if Rails.env.development? || Rails.env.test?
-    return request.host != PROVIDER_DOMAIN && request.host.include?("albratrosscms.com") if Rails.env.production?
+    render @current_website.view_path, layout: @current_website.layout_path
   end
 end
