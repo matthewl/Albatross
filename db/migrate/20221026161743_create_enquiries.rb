908d643ef518629b45cc8269be3fea25fb6e77bb
Matthew Lang <matthewlang@hey.com>
2022-10-26 22:34:15 +0100

    Re-create enquiries table

--- db/migrate/20221026161743_create_enquiries.rb
+++ db/migrate/20221026161743_create_enquiries.rb
@@ -3,19 +3,10 @@ class CreateEnquiries < ActiveRecord::Migration[7.0]
     create_table :enquiries do |t|
       t.references :website, null: false, foreign_key: true
       t.references :enquiry_form, null: false, foreign_key: true
-      t.string :title
-      t.string :first_name
-      t.string :last_name
-      t.string :premise_number
-      t.string :thoroughfare
-      t.string :locality
-      t.string :region
-      t.string :post_code
-      t.string :telephone_number
-      t.string :mobile_number
+      t.string :contact_name
+      t.string :contact_number
       t.string :email
       t.string :email_confirmation
-      t.string :enquiry_type
       t.text :message
       t.timestamps
     end
