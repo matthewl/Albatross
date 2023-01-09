7df291ff0235f631df33fe4f965982ee3cc27e9c
Matthew Lang <matthewlang@hey.com>
2023-01-09 22:45:42 +0000

    Add component for title in links theme

---
+++ test/components/themes/links/title_component_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Themes::Links::TitleComponentTest < ViewComponent::TestCase
+  test "renders when title is present" do
+    title = "Recent News"
+    sub_title = "All the recent news from your golf club"
+
+    assert_match(
+      /Recent News/,
+      render_inline(Themes::Links::TitleComponent.new(title: title, sub_title: sub_title)).to_html
+    )
+
+    assert_match(
+      /All the recent news from your golf club/,
+      render_inline(Themes::Links::TitleComponent.new(title: title, sub_title: sub_title)).to_html
+    )
+  end
+end
