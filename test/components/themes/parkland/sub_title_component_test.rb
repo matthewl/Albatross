fcdb17a251fb6bea40c21a557101b4dc686f8c38
Matthew Lang <matthewlang@hey.com>
2023-01-09 21:55:54 +0000

    Add component for sub-title in parkland theme (#69)
    Add a sub-title component for the parkland theme that can be reused across multiple components for the theme.

---
+++ test/components/themes/parkland/sub_title_component_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Themes::Parkland::SubTitleComponentTest < ViewComponent::TestCase
+  test "renders when sub-title is present" do
+    sub_title = "All the recent news from your golf club"
+    assert_match(
+      /All the recent news from your golf club/,
+      render_inline(Themes::Parkland::SubTitleComponent.new(sub_title: sub_title)).to_html
+    )
+  end
+end
