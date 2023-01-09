bee2b19889b2f6591779f8dd6315bbcf7c951383
Matthew Lang <matthewlang@hey.com>
2023-01-09 10:26:20 +0000

    Add sub-title component for links theme

--- app/components/themes/links/sub_title_component.rb
+++ app/components/themes/links/sub_title_component.rb
@@ -1,1 +1,11 @@
+# frozen_string_literal: true
 
+class Themes::Links::SubTitleComponent < ViewComponent::Base
+  def initialize(sub_title:)
+    @sub_title = sub_title
+  end
+
+  def render?
+    @sub_title.present?
+  end
+end
