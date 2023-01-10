b9e4444773b368e8ee5a06a71fa7ba18ddbd2f0f
Matthew Lang <matthewlang@hey.com>
2023-01-10 18:16:30 +0000

    Add title component for desert theme

---
+++ test/components/themes/desert/title_component_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Themes::Desert::TitleComponentTest < ViewComponent::TestCase
+  test "renders when title is present" do
+    title = "Recent News"
+    sub_title = "All the recent news from your golf club"
+
+    assert_match(
+      /Recent News/,
+      render_inline(Themes::Desert::TitleComponent.new(title: title, sub_title: sub_title)).to_html
+    )
+
+    assert_match(
+      /All the recent news from your golf club/,
+      render_inline(Themes::Desert::TitleComponent.new(title: title, sub_title: sub_title)).to_html
+    )
+  end
+
+  test "does not render when title is not present" do
+    title = ""
+    sub_title = "All the recent news from your golf club"
+
+    refute_match(
+      /All the recent news from your golf club/,
+      render_inline(Themes::Desert::TitleComponent.new(title: title, sub_title: sub_title)).to_html
+    )
+  end
+end
